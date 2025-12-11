8fcb38f3ec3e4f8666a8a17fff587d4d0e200b3b
addi-4th <gamingisscience@gmail.com>
2025-12-11 05:53:07 -0600

    more error fixes for github!!!

--- tools/ci/build.ps1
+++ tools/ci/build.ps1
@@ -1,7 +1,16 @@
-if(!(Test-Path -Path "C:/byond")){
-    bash tools/ci/download_byond.sh
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("C:/byond.zip", "C:/")
-    Remove-Item C:/byond.zip
+if (!(Test-Path -Path "C:/byond")) {
+	if (Test-Path -Path "tools/ci/byond_working-win.zip") {
+		Write-Host "Using local BYOND zip"
+		Copy-Item "tools/ci/byond_working-win.zip" "C:/byond.zip"
+	}
+	else {
+		bash tools/ci/download_byond.sh
+		if (Test-Path -Path "byond.zip") {
+			Move-Item "byond.zip" "C:/byond.zip"
+		}
+	}
+	[System.IO.Compression.ZipFile]::ExtractToDirectory("C:/byond.zip", "C:/")
+	Remove-Item C:/byond.zip
 }
 
 bash tools/ci/install_node.sh
