cddea414aaf1b184a94739dedb170eaa4c5b1206
addi-4th <gamingisscience@gmail.com>
2025-12-11 05:33:21 -0600

    fixes sum things

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 md5sum -c - <<< "4bb630000df21db3cd50bec865d7d792 *html/changelogs/example.yml"
-python3 tools/ss13_genchangelog.py html/changelogs
+python3 tools/changelog/ss13_genchangelog.py html/changelogs
